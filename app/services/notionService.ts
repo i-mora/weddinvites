f8754231dfc2e1eba049ccf9a023069c13159121
Cesar_Serna <cesarg_sv@hotmail.com>
2024-01-20 22:18:55 -0600

    wedInv/Fix: Delete AppSetings File and change for env property

--- app/services/notionService.ts
+++ app/services/notionService.ts
@@ -1,15 +1,14 @@
 import { Client, LogLevel } from '@notionhq/client'
 import { type QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints'
-import { DATABASE_ID, INTEGRATION_SECRET } from '../appSettings'
 
 const notion = new Client({
-  auth: INTEGRATION_SECRET,
+  auth: process.env.INTEGRATION_SECRET || '',
   logLevel: LogLevel.DEBUG,
 })
 
 export default async function GETUserInfo(code: string) {
   const res: QueryDatabaseResponse = await notion.databases.query({
-    database_id: DATABASE_ID,
+    database_id: process.env.DATABASE_ID || '',
     filter: {
       property: 'Code',
       rich_text: {
